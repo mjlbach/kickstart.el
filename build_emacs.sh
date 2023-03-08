92a601a2bd874b9022a670ce1aaf8c588ea2c036
Michael Lingelbach <m.j.lbach@gmail.com>
2023-03-08 01:09:13 -0800

    Update build script

--- build_emacs.sh
+++ build_emacs.sh
@@ -1,3 +1,4 @@
+# sudo dnf install texinfo gtk3-devel glib2-devel webkit2gtk4.1-devel libgccjit-devel gnutls-devel libtree-sitter-devel
 git clone --depth=1 https://git.savannah.gnu.org/git/emacs.git
 cd emacs
 ./autogen.sh
@@ -13,4 +14,7 @@ cd emacs
 	--with-sound=alsa \
 	--with-xwidgets \
 	--with-tree-sitter \
-	--without-compress-install
+	--with-mailutils \
+	--without-compress-install \
+	--prefix $HOME/.local/emacs 
+make -j install
