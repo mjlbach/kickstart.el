95358a21c07e0603a59f59e54abe7574e03a3dcd
Bruce D'Arcus <bdarcus@gmail.com>
2023-03-18 15:22:22 -0400

    Bump elpaca version

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; Install package manager
-(defvar elpaca-installer-version 0.2)
+(defvar elpaca-installer-version 0.3)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
